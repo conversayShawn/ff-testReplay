b454cba26f13721399d7454a941393034d7286fb
conversayShawn <shawn.harris3510@gmail.com>
2023-07-07 10:50:54 -0700

    initial commit

---
+++ cypress.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require("cypress");
+const fs = require('fs')
+
+module.exports = defineConfig({
+  projectId:"yrin2w",
+  e2e: {
+    setupNodeEvents(on, config) {
+      on("after:spec", (spec, results) => {
+        // if (Cypress.isBrowser('firefox')) {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some((test) =>
+            test.attempts.some((attempt) => attempt.state === "failed")
+          );
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            fs.unlinkSync(results.video);
+          }
+        }
+      });
+    },
+  },
+});
