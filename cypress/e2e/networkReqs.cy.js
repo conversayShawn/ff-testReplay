5321e5637090096c4400df174267ab5e53fde660
conversayShawn <shawn.harris3510@gmail.com>
2023-07-11 21:22:37 -0700

    code cleanup

--- cypress/e2e/networkReqs.cy.js
+++ cypress/e2e/networkReqs.cy.js
@@ -1,56 +1,64 @@
 /// <reference types="cypress" />
 
-describe('Status code test', () => {
-  it('should return a 200 status code', () => {
+describe("Status code test", () => {
+  it("should return a 200 status code", () => {
     cy.request({
-      method: 'GET',
-      url: 'https://example.cypress.io/todo',
+      method: "GET",
+      url: "https://example.cypress.io/todo",
       failOnStatusCode: false,
     }).then((response) => {
       expect(response.status).to.equal(200);
     });
   });
-  it('Failure: should fail if 200 status code', () => {
+
+    // This test should fail if a 200 response status is returned
+  it("Failure: should fail if 200 status code", () => {
     cy.request({
-      method: 'GET',
-      url: 'https://example.cypress.io/todo',
+      method: "GET",
+      url: "https://example.cypress.io/todo",
       failOnStatusCode: false,
     }).then((response) => {
       expect(response.status).to.not.equal(200);
     });
   });
-  it('should return a 200 status code', () => {
+
+  it("should return a 200 status code", () => {
     cy.request({
-      method: 'GET',
-      url: 'https://example.cypress.io/todo',
+      method: "GET",
+      url: "https://example.cypress.io/todo",
       failOnStatusCode: false,
     }).then((response) => {
       expect(response.status).to.equal(200);
     });
-  });it('should return a 200 status code', () => {
+  });
+
+  it("should return a 200 status code", () => {
     cy.request({
-      method: 'GET',
-      url: 'https://example.cypress.io/todo',
+      method: "GET",
+      url: "https://example.cypress.io/todo",
       failOnStatusCode: false,
     }).then((response) => {
       expect(response.status).to.equal(200);
     });
-  });it('should return a 200 status code', () => {
+  });
+
+  it("should return a 200 status code", () => {
     cy.request({
-      method: 'GET',
-      url: 'https://example.cypress.io/todo',
+      method: "GET",
+      url: "https://example.cypress.io/todo",
       failOnStatusCode: false,
     }).then((response) => {
       expect(response.status).to.equal(200);
     });
-  });it('should return a 200 status code', () => {
+  });
+
+  it("should return a 200 status code", () => {
     cy.request({
-      method: 'GET',
-      url: 'https://example.cypress.io/todo',
+      method: "GET",
+      url: "https://example.cypress.io/todo",
       failOnStatusCode: false,
     }).then((response) => {
       expect(response.status).to.equal(200);
     });
   });
 });
-
